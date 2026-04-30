a9cb7b25a8b5b43d156e2b20d870cd4ff0a5c872
yassine007C <104086968+yassine007C@users.noreply.github.com>
2026-04-30 01:28:54 +0100

    Update health.ts

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -3,7 +3,7 @@ import { HealthCheckResponse } from "@workspace/api-zod";
 
 const router: IRouter = Router();
 
-router.get("/healthz", (_req, res) => {
+router.get("/", (_req, res) => {
   const data = HealthCheckResponse.parse({ status: "ok" });
   res.json(data);
 });
